214afb0d44c6dc0ef0e7a8d4aec19447eb288f22
JoyceAja <ajagbe.joyce@gmail.com>
2018-09-16 23:28:35 -0400

    added new tests

--- testing/src/App.js
+++ testing/src/App.js
@@ -6,13 +6,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="encloser">
+          <input type="text" />
+          <button type="submit">Ask the God's of the internet</button>
+        </div>
       </div>
     );
   }
--- testing/src/App.test.js
+++ testing/src/App.test.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { shallow } from 'enzyme';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
 import App from './App';
+
+describe('App (Snapshot)', () => {
+  it('App renders input and button', () => {
+    // const tree = renderer.create(<App />) //why do we need this??
+    // const json = tree.toJSON()
+    const wrapper = shallow(<App />)
+    expect(wrapper).toMatchSnapshot()
+  })
+})
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('should display a text input to fill question', () =>{
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('input[type="text"]').length).toEqual(1);
+});
+
+it('should display a submit button to send question to the Internet Gods', () =>{
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('button[type="submit"]').length).toEqual(1);
+} )
